da1e755752559169d1017c3147d6591ab7428a9d
satty26 <65654262+satty26@users.noreply.github.com>
2021-01-22 15:04:51 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
+function makeTimer() {
 
+		var endTime = new Date("07 February 2021 00:00:00 GMT+05:30");
+			endTime = (Date.parse(endTime) / 1000);
+
+			var now = new Date();
+			now = (Date.parse(now) / 1000);
+
+			var timeLeft = endTime - now;
+
+			var days = Math.floor(timeLeft / 86400);
+			var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+			var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
+			var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+
+			if (hours < "10") { hours = "0" + hours; }
+			if (minutes < "10") { minutes = "0" + minutes; }
+			if (seconds < "10") { seconds = "0" + seconds; }
+
+			$(".days").html(days + "<span class='time-span'>Days</span>");
+			$(".hours").html(hours + "<span class='time-span'>Hours</span>");
+			$(".minutes").html(minutes + "<span class='time-span'>Minutes</span>");
+			$(".seconds").html(seconds + "<span class='time-span'>Seconds</span>");
+
+	}
+
+	setInterval(function() { makeTimer(); }, 1000);
 (function() {
 
   'use strict';
