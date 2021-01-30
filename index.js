7d743606e2188599c6c2f1d460414c239d3a467b
satty26 <65654262+satty26@users.noreply.github.com>
2021-01-30 12:45:55 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,61 +1,43 @@
-AOS.init();
-function makeTimer() {
 
-		var endTime = new Date("07 February 2021 00:00:00 GMT+05:30");
-			endTime = (Date.parse(endTime) / 1000);
+$(document).ready(function() {
 
-			var now = new Date();
-			now = (Date.parse(now) / 1000);
+    setTimeout(function(){
+        $('body').addClass('loaded');
+        $('h1').css('color','#fdca40');
+        AOS.init();
+        (function() {
 
-			var timeLeft = endTime - now;
+          'use strict';
 
-			var days = Math.floor(timeLeft / 86400);
-			var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
-			var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
-			var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+          // define variables
+          var items = document.querySelectorAll(".timeline li");
 
-			if (hours < "10") { hours = "0" + hours; }
-			if (minutes < "10") { minutes = "0" + minutes; }
-			if (seconds < "10") { seconds = "0" + seconds; }
+          // check if an element is in viewport
+          // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+          function isElementInViewport(el) {
+            var rect = el.getBoundingClientRect();
+            return (
+              rect.top >= 0 &&
+              rect.left >= 0 &&
+              rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+              rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+            );
+          }
 
-			$(".days").html(days + "<span class='time-span'>Days</span>");
-			$(".hours").html(hours + "<span class='time-span'>Hours</span>");
-			$(".minutes").html(minutes + "<span class='time-span'>Minutes</span>");
-			$(".seconds").html(seconds + "<span class='time-span'>Seconds</span>");
+          function callbackFunc() {
+            for (var i = 0; i < items.length; i++) {
+              if (isElementInViewport(items[i])) {
+                items[i].classList.add("in-view");
+              }
+            }
+          }
 
-	}
+          // listen for events
+          window.addEventListener("load", callbackFunc);
+          window.addEventListener("resize", callbackFunc);
+          window.addEventListener("scroll", callbackFunc);
 
-	setInterval(function() { makeTimer(); }, 1000);
-(function() {
+        })();
+    }, 3000);
 
-  'use strict';
-
-  // define variables
-  var items = document.querySelectorAll(".timeline li");
-
-  // check if an element is in viewport
-  // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
-  function isElementInViewport(el) {
-    var rect = el.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  }
-
-  function callbackFunc() {
-    for (var i = 0; i < items.length; i++) {
-      if (isElementInViewport(items[i])) {
-        items[i].classList.add("in-view");
-      }
-    }
-  }
-
-  // listen for events
-  window.addEventListener("load", callbackFunc);
-  window.addEventListener("resize", callbackFunc);
-  window.addEventListener("scroll", callbackFunc);
-
-})();
+});
